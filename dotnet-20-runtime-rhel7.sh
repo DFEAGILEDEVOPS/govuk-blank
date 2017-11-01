d6ed6c52ab5c84f8785215e31e8e0c9b1d6fff0c
Simon Massey <simon@simonmassey.org>
2017-11-01 21:07:11 +0000

    added

--- dotnet-20-runtime-rhel7.sh
+++ dotnet-20-runtime-rhel7.sh
@@ -5,8 +5,7 @@ echo oc_openshift_credentials=$oc_openshift_credentials
 echo oc_dfe_deploy_url=$oc_dfe_deploy_url
 echo oc_nexus_repo=$oc_nexus_repo
 echo oc_project=$oc_project
-
-
+echo oc_app=$oc_app
 
 echo BUILD_ARTIFACTSTAGINGDIRECTORY=$BUILD_ARTIFACTSTAGINGDIRECTORY
 echo BUILD_BINARIESDIRECTORY=$BUILD_BINARIESDIRECTORY
@@ -28,8 +27,8 @@ echo ### ADDING Dockerfile TO $INNER_ZIP
 zip $INNER_ZIP Dockerfile
 mv $INNER_ZIP $BUILD_BUILDID.zip
 
-echo ### PUBLISHING $INNER_ZIP TO $oc_nexus_repo
-curl -k -v -u $oc_nexus_credentials --upload-file $BUILD_BUILDID.zip $oc_nexus_repo
+#echo ### PUBLISHING $INNER_ZIP TO $oc_nexus_repo
+#curl -k -v -u $oc_nexus_credentials --upload-file $BUILD_BUILDID.zip $oc_nexus_repo
 
 # Download oc
 curl -u $oc_nexus_credentials -O -k https://nexus.demo.dfe.secnix.co.uk/repository/dfe_admin/oc-3.6.173.0.49-linux.tar
@@ -40,4 +39,4 @@ echo ### LOGGING IN
 
 echo ### RUNNIGN BUILD appname IN $OC_PROJECT WITH $BUILD_BUILDID.zip
 ./oc project $OC_PROJECT
-./oc start-build appname -n $OC_PROJECT --from-archive=$BUILD_BUILDID.zip
+./oc start-build $oc_app -n $OC_PROJECT --from-archive=$BUILD_BUILDID.zip
