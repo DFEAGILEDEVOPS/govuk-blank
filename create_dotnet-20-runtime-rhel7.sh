2d871a77a701f4611851f8ee2e0caa092d811aa1
Simon Massey <simon@simonmassey.org>
2017-11-01 22:12:49 +0000

    create_dotnet-20-runtime-rhel7.sh

--- create_dotnet-20-runtime-rhel7.sh
+++ create_dotnet-20-runtime-rhel7.sh
@@ -4,11 +4,11 @@ echo PROJECT_NAME=$PROJECT_NAME
 APP_NAME=$2
 echo APP_NAME=$APP_NAME
 
-oc new-project $PROJECT_NAME
-oc new-app --name=$APP_NAME dotnet:2.0~https://github.com/DFEAGILEDEVOPS/govuk-blank.git
-oc create route edge --service=$APP_NAME --hostname=$APP_NAME.demo.dfe.secnix.co.uk
-oc delete bc $APP_NAME -n $PROJECT_NAME
-oc create -f - <<EOF 
+./oc new-project $PROJECT_NAME
+./oc new-app --name=$APP_NAME dotnet:2.0~https://github.com/DFEAGILEDEVOPS/govuk-blank.git
+./oc create route edge --service=$APP_NAME --hostname=$APP_NAME.demo.dfe.secnix.co.uk
+./oc delete bc $APP_NAME -n $PROJECT_NAME
+./oc create -f - <<EOF 
 apiVersion: v1
 kind: BuildConfig
 metadata:
