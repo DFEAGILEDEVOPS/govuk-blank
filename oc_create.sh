d8e6e72d09c393fff98a13771796dde96b34e3b2
Simon Massey <simon@simonmassey.org>
2017-11-01 21:52:44 +0000

    oc_create.sh

--- oc_create.sh
+++ oc_create.sh
@@ -1,7 +1,9 @@
+#"$(oc_project_name)" "$(oc_build_name)" "$(oc_runtime_image)"
 oc_project_name=$1
 oc_build_name=$2
 oc_runtime_image=$3
 echo oc_project_name=$oc_project_name
 echo oc_build_name=$oc_build_name
 echo oc_runtime_image=$oc_runtime_image
-./${oc_runtime_image}.sh $oc_project_name $oc_build_name
+echo source ${oc_runtime_image}.sh $oc_project_name $oc_build_name
+source ${oc_runtime_image}.sh $oc_project_name $oc_build_name
