dd77f403fc78b4af3b196c1c3c22a09cc23e1bc7
Simon Massey <simon@simonmassey.org>
2017-11-06 18:46:31 +0000

    oc from dropbox

--- dotnet-20-runtime-rhel7.sh
+++ dotnet-20-runtime-rhel7.sh
@@ -27,12 +27,14 @@ echo ### ADDING Dockerfile TO $INNER_ZIP
 zip $INNER_ZIP Dockerfile
 mv $INNER_ZIP $BUILD_BUILDID.zip
 
-echo ### PUBLISHING $INNER_ZIP TO $oc_nexus_repo
-curl -k -v -u $oc_nexus_credentials --upload-file $BUILD_BUILDID.zip $oc_nexus_repo
+#echo ### PUBLISHING $INNER_ZIP TO $oc_nexus_repo
+#curl -k -v -u $oc_nexus_credentials --upload-file $BUILD_BUILDID.zip $oc_nexus_repo
 
 # Download oc
-curl -u $oc_nexus_credentials -O -k https://nexus.demo.dfe.secnix.co.uk/repository/dfe_admin/oc-3.6.173.0.49-linux.tar
-tar xfv oc-3.6.173.0.49-linux.tar
+#curl -u $oc_nexus_credentials -O -k https://nexus.demo.dfe.secnix.co.uk/repository/dfe_admin/oc-3.6.173.0.49-linux.tar
+#tar xfv oc-3.6.173.0.49-linux.tar
+wget -O oc.tar https://www.dropbox.com/s/ir3xms1m72p5lsh/oc-3.6.173.0.49-linux.tar?dl=0
+tar xfv oc.tar
 
 echo ### LOGGING IN
 ./oc login --insecure-skip-tls-verify https://demo.dfe.secnix.co.uk:8443 --token="$oc_openshift_credentials"
